b9c71229b36715c90c7f1d246b1e063808085b2f
Trungli <eadiez.u@gmail.com>
2022-07-09 19:38:41 -0400

    zombie setup

--- dyn4j_test/src/tests/BAOLauncher.java
+++ dyn4j_test/src/tests/BAOLauncher.java
@@ -3,7 +3,14 @@ package tests;
 public class BAOLauncher {
 	public static void main(String[] args) {
 		BAOSimulationFrame game = new BAOSimulationFrame("Bolt Action Outbreak", 10);
-		new PlayerGameObject(0, game, "player");
+		PlayerGameObject player = new PlayerGameObject(0, game, "player");
+		new ZombieGameObject(1, game, "zombie1", -4.2, 4, player);
+
+		new ZombieGameObject(1, game, "zombie1", -5.2, 4, player);
+
+		new ZombieGameObject(1, game, "zombie1", -1.2, 4, player);
+
+		new ZombieGameObject(1, game, "zombie1", 4.2, 4, player);
 		game.run();
 	}
 }
--- dyn4j_test/src/tests/ZombieGameObject.java
+++ dyn4j_test/src/tests/ZombieGameObject.java
@@ -1,16 +1,32 @@
 package tests;
 
+import dyn4j.geometry.Geometry;
+import dyn4j.geometry.MassType;
+import dyn4j.geometry.Vector2;
+import framework.SimulationBody;
+
 public class ZombieGameObject extends GameObject {
 
-	public ZombieGameObject(int id, BAOSimulationFrame frame, String name) {
+	SimulationBody zombie;
+	double initialX;
+	double initialY;
+	PlayerGameObject player;
+	
+	public ZombieGameObject(int id, BAOSimulationFrame frame, String name, double x, double y, PlayerGameObject player) {
 		super(id, frame, name);
-		// TODO Auto-generated constructor stub
+		this.player = player;
+		initialX = x;
+		initialY = y;
 	}
 
 	@Override
 	public void initialize() {
 		// TODO Auto-generated method stub
-
+		zombie = new SimulationBody();
+		zombie.addFixture(Geometry.createCircle(1.0));
+		zombie.translate(new Vector2(initialX, initialY));
+		zombie.setMass(MassType.NORMAL);
+		frame.world.addBody(zombie);
 	}
 
 	@Override
