45e9d29f6675fadc1705214b080d84fe4f7e9667
wdharter <wdharter@outlook.com>
2022-07-12 18:06:06 -0400

    First pass SoundManager
    First implementation of the sound manager

--- dyn4j_test/src/tests/SoundManager.java
+++ dyn4j_test/src/tests/SoundManager.java
@@ -1,14 +1,169 @@
 package tests;
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
 
 public class SoundManager {
 
-	//static reference to sound names
-	//struct with sound names
+	Long currentFrame;
+	Clip clip;
+	String status;
+	AudioInputStream audioInputStream;
+	Sound s;
 	
-	public SoundManager() {
-		// TODO Auto-generated constructor stub
-	}
+	public SoundManager(Sound s)
+			throws UnsupportedAudioFileException, 
+			IOException, LineUnavailableException
+	{
+		this.s = s;
+		switch(s)
+		{
+			case UNLOCK:
+				audioInputStream = AudioSystem.getAudioInputStream(new File("Unlock.wav").getAbsoluteFile());
+				break;
+			case OPEN:
+				audioInputStream = AudioSystem.getAudioInputStream(new File("Open.wav").getAbsoluteFile());
+				break;
+			case CLOSE:
+				audioInputStream = AudioSystem.getAudioInputStream(new File("Close.wav").getAbsoluteFile());
+				break;
+			case LOCK:
+				audioInputStream = AudioSystem.getAudioInputStream(new File("Lock.wav").getAbsoluteFile());
+				break;
+			case FIRE:
+				audioInputStream = AudioSystem.getAudioInputStream(new File("Fire.wav").getAbsoluteFile());
+				break;
+			case AIM:
+				audioInputStream = AudioSystem.getAudioInputStream(new File("Aim.wav").getAbsoluteFile());
+				break;
+			case ZOMBIE:
+				audioInputStream = AudioSystem.getAudioInputStream(new File("Zombie.wav").getAbsoluteFile());
+				break;
+			case ZOMBIE_WALK:
+				audioInputStream = AudioSystem.getAudioInputStream(new File("Zombie_Walk.wav").getAbsoluteFile());
+				break;
+			case PLAYER_WALK:
+				audioInputStream = AudioSystem.getAudioInputStream(new File("Player_Walk.wav").getAbsoluteFile());
+				break;
+			case DAMAGE_2_ZOMBIE:
+				audioInputStream = AudioSystem.getAudioInputStream(new File("Damage_2_Zombie.wav").getAbsoluteFile());
+				break;
+			case DAMAGE_2_PLAYER:
+				audioInputStream = AudioSystem.getAudioInputStream(new File("Damage_2_Player.wav").getAbsoluteFile());
+				break;
+			case IT_HAS_TO_BE:
+				audioInputStream = AudioSystem.getAudioInputStream(new File("It_Has_To_Be.wav").getAbsoluteFile());
+				break;
+			case AMBIANCE:
+				audioInputStream = AudioSystem.getAudioInputStream(new File("Ambiance.wav").getAbsoluteFile());
+				break;
+		}
+		
+		clip = AudioSystem.getClip();
+		clip.open(audioInputStream);
+		
+		}
+	    public void play() 
+	    {
+	        clip.start();     
+	        status = "play";
+	    }
+	    public void pause() 
+	    {
+	        if (status.equals("paused")) 
+	        {
+	            System.out.println("audio is already paused");
+	            return;
+	        }
+	        this.currentFrame = 
+	        this.clip.getMicrosecondPosition();
+	        clip.stop();
+	        status = "paused";
+	    }
+		public void restart() throws IOException, LineUnavailableException,
+		UnsupportedAudioFileException 
+		{
+		clip.stop();
+		clip.close();
+		resetAudioStream(this.s);
+		currentFrame = 0L;
+		clip.setMicrosecondPosition(0);
+		this.play();
+		}
 	
-	//static function that takes in soundname struct and plays that sound
-
+	    public void stop() throws UnsupportedAudioFileException,
+	    IOException, LineUnavailableException 
+	    {
+	        currentFrame = 0L;
+	        clip.stop();
+	        clip.close();
+	    }
+	
+	    public void resetAudioStream(Sound s) throws UnsupportedAudioFileException, IOException,
+	    LineUnavailableException 
+		{
+	    	switch(s)
+			{
+				case UNLOCK:
+					audioInputStream = AudioSystem.getAudioInputStream(new File("Unlock.wav").getAbsoluteFile());
+					break;
+				case OPEN:
+					audioInputStream = AudioSystem.getAudioInputStream(new File("Open.wav").getAbsoluteFile());
+					break;
+				case CLOSE:
+					audioInputStream = AudioSystem.getAudioInputStream(new File("Close.wav").getAbsoluteFile());
+					break;
+				case LOCK:
+					audioInputStream = AudioSystem.getAudioInputStream(new File("Lock.wav").getAbsoluteFile());
+					break;
+				case FIRE:
+					audioInputStream = AudioSystem.getAudioInputStream(new File("Fire.wav").getAbsoluteFile());
+					break;
+				case AIM:
+					audioInputStream = AudioSystem.getAudioInputStream(new File("Aim.wav").getAbsoluteFile());
+					break;
+				case ZOMBIE:
+					audioInputStream = AudioSystem.getAudioInputStream(new File("Zombie.wav").getAbsoluteFile());
+					break;
+				case ZOMBIE_WALK:
+					audioInputStream = AudioSystem.getAudioInputStream(new File("Zombie_Walk.wav").getAbsoluteFile());
+					break;
+				case PLAYER_WALK:
+					audioInputStream = AudioSystem.getAudioInputStream(new File("Player_Walk.wav").getAbsoluteFile());
+					break;
+				case DAMAGE_2_ZOMBIE:
+					audioInputStream = AudioSystem.getAudioInputStream(new File("Damage_2_Zombie.wav").getAbsoluteFile());
+					break;
+				case DAMAGE_2_PLAYER:
+					audioInputStream = AudioSystem.getAudioInputStream(new File("Damage_2_Player.wav").getAbsoluteFile());
+					break;
+				case IT_HAS_TO_BE:
+					audioInputStream = AudioSystem.getAudioInputStream(new File("It_Has_To_Be.wav").getAbsoluteFile());
+					break;
+				case AMBIANCE:
+					audioInputStream = AudioSystem.getAudioInputStream(new File("Ambiance.wav").getAbsoluteFile());
+					break;
+			}
+	        clip.open(audioInputStream);
+		}
+}
+enum Sound{
+	UNLOCK,
+	OPEN,
+	CLOSE,
+	LOCK,
+	FIRE,
+	AIM,
+	ZOMBIE,
+	ZOMBIE_WALK,
+	PLAYER_WALK,
+	DAMAGE_2_ZOMBIE,
+	DAMAGE_2_PLAYER,
+	IT_HAS_TO_BE,
+	AMBIANCE
 }
