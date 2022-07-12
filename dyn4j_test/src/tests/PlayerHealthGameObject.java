3e8d8f9e470f94caa0e089d3efcd99aaf0210b1f
Trungli <eadiez.u@gmail.com>
2022-07-12 18:07:01 -0400

    Adding sample, sounds and starting health

---
+++ dyn4j_test/src/tests/PlayerHealthGameObject.java
@@ -0,0 +1,36 @@
+package tests;
+
+import java.awt.Graphics2D;
+
+public class PlayerHealthGameObject extends GameObject {
+
+	public PlayerHealthGameObject(int id, BAOSimulationFrame frame, String name) {
+		super(id, frame, name);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void initialize() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void render(Graphics2D g, double elapsedTime) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void handleEvents() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
