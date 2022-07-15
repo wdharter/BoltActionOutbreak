26b107df580af05426681b65b2ed88959f413d2d
Trungli <eadiez.u@gmail.com>
2022-07-15 15:20:46 -0400

    Muzzle flash fix

---
+++ dyn4j_test/src/baologic/AnimationManagerGameObject.java
@@ -0,0 +1,36 @@
+package baologic;
+
+import java.awt.Graphics2D;
+
+public class AnimationManagerGameObject extends GameObject {
+
+	public AnimationManagerGameObject(int id, BAOSimulationFrame frame, String name) {
+		super(id, frame, name);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void initialize() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void render(Graphics2D g, double elapsedTime) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void handleEvents() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
